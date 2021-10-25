3713da9b88e64e87cba4f2a988305feaae6d8a6e
Henry Pontzer <55723056+Spyromaniac31@users.noreply.github.com>
2021-10-25 15:38:10 -0600

    Update script.sh
    Added shebang

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sed 's/pam_cracklib.so/pam_cracklib.so ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1 /g' /etc/pam.d/common-password
 sed 's/pam_unix.so/pam_unix.so remember=5 minlen=8 /g' /etc/pam.d/common-password
