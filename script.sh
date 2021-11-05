c5db9701dc6817b893dfd05c6d65ec02841704f8
Henry Pontzer <55723056+Spyromaniac31@users.noreply.github.com>
2021-11-05 14:49:03 -0600

    Update script.sh
    Updated sed syntax

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ufw enable
 echo 'allow-guest=false' >> /etc/lightdm/lightdm.conf
-sed 's/pam_cracklib.so/pam_cracklib.so ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1 /g' /etc/pam.d/common-password
-sed 's/pam_unix.so/pam_unix.so remember=5 minlen=8 /g' /etc/pam.d/common-password
+sed -i 's/pam_cracklib.so/pam_cracklib.so ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1 /' /etc/pam.d/common-password
+sed -i 's/pam_unix.so/pam_unix.so remember=5 minlen=8 /' /etc/pam.d/common-password
 echo 'auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800' >> /etc/pam.d/common-auth
